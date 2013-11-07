d2dbde99620396e2fbebc9c86bb586c2feca0268
Petter H. Juliussen <post@petterhj.net>
2013-11-07 09:19:02 +0000

    big update

--- oblig5/Oblig5.java
+++ oblig5/Oblig5.java
@@ -1,6 +1,7 @@
 // Imports
 import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,13 +11,21 @@ import java.util.Scanner;
 class Oblig5 {
     // Main
     public static void main(String[] args) {
+        final String RD = "\u001B[31m";
+        final String GN = "\u001B[32m";
+        final String YL = "\u001B[33m";
+        final String RT = "\u001B[0m";
+        
 		// Header
-		System.out.println("============================================");
-		System.out.println(" Reiseplanlegger");	
-		System.out.println("============================================\n");
+		System.out.println("\n=============================================================================");
+		System.out.println(" "+RD+"Reiseplanlegger"+RT);
+		System.out.println("=============================================================================\n");
 		
 		// Initiate planner
         Planner pl = new Planner();
+        
+        // Footer (spacer)
+        System.out.println();
     }
 }
 
@@ -25,9 +34,10 @@ class Oblig5 {
 // =================================================================================
 class Planner {
     // Variables
-    private final String DATAFILE = "trikkogtbaneutf8.txt";
+    //private final String DATAFILE = "trikkogtbaneutf8-ver2.txt";
+    private final String DATAFILE = "testdata.txt";
 	
-	//private ArrayList<Station> stations;
+    private HashMap<Integer, Line> lines = new HashMap<Integer, Line>();
 	private HashMap<String, Station> stations = new HashMap<String, Station>();
     
     // Constructor
@@ -36,7 +46,7 @@ class Planner {
         this.parseData();
         
         // Get user origin/destination
-        //this.queryFromTo();
+        this.queryFromTo();
     }
     
     // Parse data file
@@ -44,35 +54,54 @@ class Planner {
         try {
             BufferedReader data = new BufferedReader(new FileReader(DATAFILE));
             
-			String line;
+			String dataline;
 			int lineNumber = 0;
 			int stationNumber = 0;
 			
-            while ((line = data.readLine()) != null) {
-				// Line
-                if (line.toLowerCase().contains("*linje*")) {
-					lineNumber = Integer.parseInt(line.toLowerCase().split("\\*linje\\* ")[1]);
+            while ((dataline = data.readLine()) != null) {
+                // Line
+                if (dataline.toLowerCase().contains("*linje*")) {
+					lineNumber = Integer.parseInt(dataline.toLowerCase().split("\\*linje\\* ")[1]);
 					stationNumber = 0;
-					System.out.println(line + " -- " + lineNumber);
 					
-					// Add line
+                    // Add new line
+                    Line line = new Line();
+					line.setNumber(lineNumber);
+                    lines.put(line.getNumber(), line);
+                    
+                    // DEBUG
+                    //System.out.println("\n" + dataline + " -- " + lineNumber);
 				} 
 				// Station
 				else {
 					stationNumber++;
-					System.out.print("S" + stationNumber + " -- " + "L" + lineNumber + " -- " + line);
-					
-					// Add station
-					Station station = new Station();
-					station.setName(line);
-					
-					if (this.isStation(line)) {
-						// Add new line
-						System.out.println(" --- EXISTING");
+                    
+                    Station station;
+                    
+					if (this.isStation(dataline)) {
+						// Get existing station
+                        station = this.getStationByName(dataline);
+                        
+                        // DEBUG
+						//System.out.print("EXT\t");
 					} else {
 						// Add new station
-						stations.put(station.getName(), station);
+                        station = new Station();
+                        station.setName(dataline);
+						stations.put(station.getName().toLowerCase(), station);
+                        
+                        // DEBUG
+                        //System.out.print("NEW\t");
 					}
+                    
+                    // Add line to station
+                    station.addLine(this.getLineByNumber(lineNumber));
+                    
+                    // Add station to line
+                    this.getLineByNumber(lineNumber).addStation(station);
+                    
+                    // DEBUG
+                    //System.out.println("S" + stationNumber + " -- " + "L" + this.getLineByNumber(lineNumber).getNumber() + " -- " + station.getLines().size() + " --- " + station.getName());
 				}				
             }
             
@@ -88,59 +117,132 @@ class Planner {
     // Query 
     private void queryFromTo() {
 		// User input
-        Scanner input = new Scanner(System.in);
-        
         System.out.println(" Hvor skal vi reise hen?");
-        System.out.print("  > Fra: ");
         
-        String origin = input.nextLine().trim();
+        Station origin = this.getStationByName(this.getAndValidateInput("Fra"));
+        Station destination = this.getStationByName(this.getAndValidateInput("Til"));
         
-        System.out.print("  > Til: ");
+        //Station origin = this.getStationByName("Skogen");
+        //Station destination = this.getStationByName("Bøler");
         
-        String destination = input.nextLine().trim();
-		
-		// Find routes
-		// TODO: VALIDATE STATION NAMES!
-		//this.findRoutes(
+        //Station origin = this.getStationByName("Sannergata");
+        //Station destination = this.getStationByName("Jernbanetorget");
+        
+        // Find routes
+        this.findRoutes(origin, destination);
+    }
+    
+    private String getAndValidateInput(String q) {
+        Scanner input = new Scanner(System.in);
+        String stationName;
+        int attempts = 0;
+        
+        do {
+            String query = ((attempts == 0) ? q + ": " : "Fant ikke stasjon, prøv igjen: ");
+            System.out.print("  > " + query);
+            stationName = input.nextLine();
+            attempts++;
+        } while (!this.isStation(stationName));
+        
+        return stationName;
     }
     
     // Find routes
     private void findRoutes(Station from, Station to) {
 		// Header
-		System.out.println(" Aktuelle reiseruter");
-		System.out.println("--------------------------------------------\n");
-		
-		// 
-        // Skriv metoden "void beregnRuter( )" som ut fra L1 og L2 finner, og får skrevet ut enten 
-        // én reisevei (punkt 3), eller en rekke reiseveier (punkt 4) fra xxx til yyy. Alle reiseveier som 
-        // finnes skrives ut som vist i punkt 3 eller punkt 4. f) 
+		System.out.println("\n\n Aktuelle reiseruter mellom " + from.getName() + " og " + to.getName());
+		System.out.println("-----------------------------------------------------------------------------\n");
+        
+        ArrayList<Line> fromLines = from.getLines();
+        ArrayList<Line> toLines = to.getLines();
+        
+        
+        // DEBUG START
+        /*
+        System.out.println("Linjer - " + from.getName() + ":");
+        for (Line l : fromLines)
+            System.out.println(" > linje " + l.getNumber() + ", stasjon nr.: " + l.getStationNumber(from));
+        
+        System.out.println("Linjer - " + to.getName() + ":");
+        for (Line l : toLines)
+            System.out.println(" > linje " + l.getNumber() + ", stasjon nr.: " + l.getStationNumber(to));
+        
+        System.out.println();
+        */
+        // DEBUG END
+        
+        // Direct routes
+        ArrayList<Line> direct = new ArrayList<Line>(fromLines);
+        direct.retainAll(toLines);
+        
+        if(!direct.isEmpty()) {
+            System.out.println(" Direkterute(r):");
+            
+            for (Line l : direct) {
+                System.out.print("   o Ta " + l.getType() + " linje " + l.getNumber() + " fra: " + from.getName());
+                System.out.println(" til " + to.getName() + " retning " + l.getEndOfLine(from, to).getName() + ".");
+                System.out.println("   o Estimert reisetid: ... .\n");
+            }
+        }
+        
+        // Indirect routes
+        else {
+            System.out.println(" Omstigningsrute(r):");
+            
+            for (Line l : fromLines) {
+                for (Station s : l.getStations()) {
+                    for (Line sl : s.getLines()) {
+                        // Found route (with only one transfer!)
+                        if(sl.hasStation(to)) {
+                            System.out.print("   o Ta " + l.getType() + " linje " + l.getNumber() + " fra: " + from.getName());
+                            System.out.println(" til " + s.getName() + " retning " + l.getEndOfLine(from, s).getName() + ",");
+                            System.out.print("   | og deretter " + sl.getType() + " linje " + sl.getNumber() + " retning " + sl.getEndOfLine(s, to).getName());
+                            System.out.println(" til " + to.getName() + ".");
+                            System.out.println("   o Estimert reisetid: ... .\n");
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    
+    // Return line (by number)
+    private Line getLineByNumber(int n) {
+        Line l = this.getLines().get(n);
+        
+        if (l != null)
+            return l;
+            
+        return null;
     }
 	
-	
-	// Return stations
-	private HashMap<String, Station> getStations() {
-		return this.stations;
+	// Return station (by name)
+	private Station getStationByName(String n) {
+        Station s = this.getStations().get(n.toLowerCase().trim());
+        
+		if (s != null)
+			return s;
+		
+		return null;
 	}
-	
-	// Check if existing station (by name)
+    
+    // Check if existing station (by name)
 	private boolean isStation(String n) {
-		if (this.getStations().get(n) == null)
+		if (this.getStations().get(n.toLowerCase().trim()) == null)
 			return false;
 		
 		return true;
 	}
-	
-	/*
-	
-	// Return station (by name)
-	private Station getStationByName(String n) {
-		for (Station s : this.getStations())
-            if(s.getName().equalsIgnoreCase(n))
-                return s;
-
-        return null;
+    
+    // Getters/Setters
+    private HashMap<Integer, Line> getLines() {
+        return this.lines;
+    }
+	private HashMap<String, Station> getStations() {
+		return this.stations;
 	}
-	*/
 }
 
 
@@ -150,29 +252,25 @@ class Line {
 	/*	Variables
 	------------------------------------ */
 	private int number;
-    private String type;
 	
-	private ArrayList<Station> stations;
+	private ArrayList<Station> stations = new ArrayList<Station>();
     
 	
 	/*	Methods
 	------------------------------------ */
 	
 	// Add station to line
-	void newStation(Station s) {
+	void addStation(Station s) {
 		stations.add(s);
 	}
 	
 	// Check if line goes through station
     boolean hasStation(Station s) {
-        // som returnerer sann hvis stasjonen st befinner seg på den linja som Linje-objektet representerer.
+        if (this.getStations().indexOf(s) < 0)
+            return false;
+
 		return true;
     }
-	
-	// 
-	String direction(Station from, Station to) {
-		return "";
-	}
 	
 	// Getters and setters
 	void setNumber(int n) {
@@ -180,6 +278,18 @@ class Line {
 	}
 	int getNumber() {
 		return this.number;
+	}
+    ArrayList<Station> getStations() {
+        return this.stations;
+    }
+    int getStationNumber(Station s) {
+        return this.getStations().indexOf(s) + 1;
+    }
+    Station getEndOfLine(Station from, Station to) {
+		if (this.getStations().indexOf(from) < this.getStations().indexOf(to))
+            return this.getStations().get(this.getStations().size() - 1);
+        else
+            return this.getStations().get(0);
 	}
 	String getType() {
 		return ((this.getNumber() < 10) ? "t-bane" : "trikk");
@@ -192,31 +302,27 @@ class Line {
 class Station {
 	/*	Variables
 	------------------------------------ */
-	private String id;
 	private String name;
-	
-	private ArrayList<Line> lines;
+    
+    private ArrayList<Line> lines = new ArrayList<Line>();
 	
 	
 	/*	Methods
 	------------------------------------ */
 	
 	// Add line to station
-	void newLine(Line l, int stationNumber) {
-		
+	void addLine(Line l) {
+		lines.add(l);
 	}
-	
-	
-	public boolean hasLine(Line l) {
-		return true;
-	}
-	
 	
 	// Getters and setters
-	public void setName(String n) {
+    ArrayList<Line> getLines() {
+        return this.lines;
+    }
+	void setName(String n) {
 		this.name = n;
 	}
-	public String getName() {
+	String getName() {
 		return this.name;
 	}
 }
@@ -225,5 +331,5 @@ class Station {
 // 	Class: Transfer
 // =================================================================================
 class Transfer {
-    
+
 }
